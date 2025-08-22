db4018a99c68c6fc064ef2b4f6d60917df52558f
5t33v3 <vsteeve45@gmail.com>
2025-08-22 13:55:33 +0530

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,38 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
+  
 })
 export class App {
-  protected readonly title = signal('todo');
+  title = "My Todo App";
+  filter : "all" | "active" | "done" = "all";
+
+  allItems = [
+    { description: "eat", done: true },
+    { description: "sleep", done: false },
+    { description: "play", done: false },
+    { description: "laugh", done: false }
+  ];
+
+  addItem(description: string){
+    if (!description) return;
+
+    this.allItems.unshift(
+      {description, done: false}
+    );
+  }
+  get items(){
+    if (this.filter === "all"){
+      return this.allItems;
+    }
+    return this.allItems.filter((item) => this.filter === "done" ? item.done : !item.done);
+  }
+
 }
